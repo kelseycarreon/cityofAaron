a9df62b1a88b91b44ba331858042b76e18524ecc
Kelsey Carreon <38888544+kelseycarreon@users.noreply.github.com>
2018-05-17 14:07:06 -0400

    Add files via upload

---
+++ findTheGold/src/cityOfAaron/FindTheGold.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+/*
+* The main() class file for the cityOfAaron project * CIT-260
+* Spring 2018
+* Team members: Marshall Evanas, Kelsey Haddow */
+package cityOfAaron; 
+
+
+/**
+ *
+ * @author khaddow
+ */
+public class FindTheGold {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
